504cacb4b862845d4c62c462a0a492f9df999b52
FerrariMarc0 <marco1010@live.it>
2023-02-20 18:16:57 +0100

    Commenti

--- js/main.js
+++ js/main.js
@@ -2,17 +2,27 @@
 
 //Richiesta nome
 const firstName = prompt('Inserisci il tuo nome');
+
 //Richiesta cognome
 const lastName = prompt('Inserisci il tuo cognome');
+
 //Richiesta età
 const age = prompt('Inserisci la tua età');
+
 //Richiesta colore preferito
-let favColor = prompt('Inserisci il tuo colore preferito');
+const favColor = prompt('Inserisci il tuo colore preferito');
+
 //impostazione variabile da inserire in html
 let classe = 89;
+
+//Assegnazione finale
+const username = `Ciao ${firstName} ${lastName} di ${age} anni, il tuo colore preferito è il ${favColor}!!`;
+
 //Inserimento dati utente in html
-document.getElementById('quest').innerHTML = `${firstName} ${lastName} ${age} ${favColor}`;
+document.getElementById('quest').innerHTML = username;
+
 //Inseritmento numero in html
 document.getElementById('go').innerHTML = classe;
+
 //Stampa in console
-console.log(`ciao ${firstName} ${lastName} di ${age} anni, il tuo colore preferito è il ${favColor}`);
+console.log(username);
